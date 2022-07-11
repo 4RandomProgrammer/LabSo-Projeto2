7022985009a889225e9ae179cc7ac7ea288ab82f
ARandomProgrammer <luis.henriques@estudante.ufscar.br>
2022-07-11 19:09:18 -0300

    oq foi feito sem conexão do lab

--- shell.c
+++ shell.c
@@ -8,44 +8,53 @@
 
 int main() {
   char comando[MAX];
-  char argv[MAX][100];
+  char *argv[MAX];
   int pid;
 
   while (1) {
     printf("> ");
     fgets(comando, MAX, stdin);
     comando[strlen(comando)-1] = '\0';
-    
+    printf("%s\n", comando);
     if (!strcmp(comando, "exit")) {
       exit(EXIT_SUCCESS);
     }
     
-    strcpy(argv[0], strtok(comando, " "));
+    strcpy(&argv[0], strtok(comando, " "));
     int argc = 1;
     char *temp_arg;
+    
     while (1) {
       temp_arg = strtok(NULL, " ");
       if (!temp_arg)
+        argv[argc] = NULL;
+        argc++;
         break;
       
-      strcpy(argv[argc], temp_arg);
+      strcpy(&argv[argc], temp_arg);
       argc++;
     }
+    //arg c diz o número de parametros enviados.
+    //printf("ARGC: %d\n", argc);
     
-    printf("ARGC: %d\n", argc);
     
-    printf("\nPRINTANDO CADA ARG\n");
-    for (int j = 0; j < argc; j++)
-      printf("Printando: %s|\n", argv[j]);
-
+    //printf("\nPRINTANDO CADA ARG\n");
+    //for (int j = 0; j < argc; j++)
+      //printf("Printando: %s|\n", argv[j]);
+    
+    
     pid = fork();
     if (pid) {
       if (argc <= 1 || argv[1][0] != '&') {
         waitpid(pid, NULL, 0);
         printf("WAITING\n");
       }
+      
     } else {
-      execlp(argv[0], argv[0], NULL);
+      //printf("%d\n",argc);
+      //printf("%s\n",argv[0]);
+
+      execvp(argv[0], argv);
       printf("Erro ao executar comando!\n");
       exit(EXIT_FAILURE);
     }
